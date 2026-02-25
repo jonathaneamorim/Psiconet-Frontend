6e4019c740e804020cab1e4248a90c23dd986d4c
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-25 16:50:13 -0300

    feat: criação da not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/Header";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div>
+            <Header />
+            <main className="bg-[var(--primary)] min-h-screen min-w-screen flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center bg-slate-100 px-10 py-8 rounded-lg shadow-lg md:px-16 md:py-12">
+                    <h1 className="text-6xl font-bold text-gray-800">404</h1>
+                    <p className="text-xl text-gray-600 mt-4">Ops! Página não encontrada.</p>
+                    <Link href="/" className="mt-6 text-blue-500 hover:text-blue-700">Voltar para a página inicial</Link>
+                </div>
+            </main>
+        </div>
+    );
+}
