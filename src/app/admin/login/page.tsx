bf7adc62a715c5999d3c7ae60218eb4b7068599a
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-26 21:39:35 -0300

    feat: implementação do login no môdulo admin

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,17 @@
-export function AdminLogin() {
+import { Header } from "@/components/Organism/Header";
+import { FormLogin } from "@/components/Organism/FormLogin";
+
+export default function AdminLogin() {
     return (
-        <div>
-            <h1>Login do Administrador</h1>
+        <div className="min-h-screen w-full flex justify-center items-center px-4 py-10">
+            <div className="relative w-full max-w-[500px] overflow-hidden flex flex-col">
+                <div className="flex-1 flex flex-col pt-10 h-full relative z-10">
+                    <Header />
+                    <main className="flex-1 flex items-center justify-center p-2">
+                        <FormLogin loginType="Admin" />
+                    </main>
+                </div>
+            </div>
         </div>
     );
 }
