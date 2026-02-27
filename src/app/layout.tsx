d7f83e95d9fd9a333c918e9a3c981fc14ff2aa20
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-27 14:02:10 -0300

    feat: inserindo toast no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { sharedMetadata } from "./shared-metadata";
 import { Raleway } from 'next/font/google';
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const raleway = Raleway({
   subsets: ['latin'],
@@ -19,6 +20,17 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={raleway.variable}>
         {children}
+        <Toaster 
+          position="top-right" 
+          toastOptions={{ 
+            duration: 4000,
+            style: {
+              maxWidth: '350px',
+              wordBreak: 'break-word', 
+            },
+            className: '!max-w-[90vw] md:!max-w-sm truncate', 
+          }}
+          />
       </body>
     </html>
   );
