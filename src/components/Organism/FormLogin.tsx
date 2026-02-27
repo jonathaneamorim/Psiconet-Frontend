54cd6b7dab410b91a4ae298bd7b1323301d205d2
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-27 14:03:14 -0300

    feat: integrando action auth com o componente de login

--- src/components/Organism/FormLogin.tsx
+++ src/components/Organism/FormLogin.tsx
@@ -1,9 +1,36 @@
+'use client';
+
 import { InputLabel } from "../Molecules/InputLabel";
 import { Button } from "../Atoms/Button";
+import { useState } from "react";
+import { loginAction } from "@/actions/auth";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export function FormLogin({loginType}: {loginType: string}) {
+    const [errorMessage, setErrorMessage] = useState("");
+    const type = loginType.toLocaleLowerCase().trim() as 'admin' | 'psicologo' | 'paciente';
+    const router = useRouter();
 
-    const type = loginType.toLocaleLowerCase().trim();
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); 
+
+        const formData = new FormData(e.currentTarget);
+        const keepLoggedIn = formData.get("keeploggedin") === "on";
+        
+        setErrorMessage("");
+        const toastId = toast.loading("Realizando login...");
+
+        const result = await loginAction(formData, type, keepLoggedIn);
+        
+        if(result?.error) {
+            setErrorMessage(result.error);
+            toast.error(result.error, { id: toastId }); 
+        } else if (result?.success) {
+            toast.success("Usuário logado!", { id: toastId });
+            router.push(result.redirectTo);
+        }
+    }
 
     return (
         <div className="w-full bg-[var(--secondary)] px-5 py-5 rounded-3xl shadow-md flex justify-center align-center flex-col gap-6">
@@ -12,10 +39,9 @@ export function FormLogin({loginType}: {loginType: string}) {
                 <p className="text-2xl">Login - {loginType}</p>
             </div>
 
-            <form className="flex flex-col gap-6">
+            <form onSubmit={handleLogin} className="flex flex-col gap-6">
                 <div className="flex flex-col gap-3">
                     <InputLabel fieldName="Email" htmlFor="email" inputType="email" />
-                    {type === "psicologo" && <InputLabel fieldName="CRP" htmlFor="password" />}
                     <InputLabel fieldName="Senha" htmlFor="password" inputType="password" />
                 </div>
 
