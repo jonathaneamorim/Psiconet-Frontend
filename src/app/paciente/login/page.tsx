96a03f1aee66101b1744090f43e98d84f58174a3
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-26 21:39:41 -0300

    feat: implementação do login no môdulo paciente

--- src/app/paciente/login/page.tsx
+++ src/app/paciente/login/page.tsx
@@ -1,11 +1,16 @@
-import { InputLabel } from "@/components/Molecules/InputLabel";
+import { FormLogin } from "@/components/Organism/FormLogin";
+import { Header } from "@/components/Organism/Header";
 
 export default function PacienteLogin() {
     return (
-        <div>
-            <h1>Login do Paciente</h1>
-            <div className="p-4">
-                <InputLabel fieldName="Email" htmlFor="email" />
+        <div className="min-h-screen w-full flex justify-center items-center px-4 py-10">
+            <div className="relative w-full max-w-[500px] overflow-hidden flex flex-col">
+                <div className="flex-1 flex flex-col pt-10 h-full relative z-10">
+                    <Header />
+                    <main className="flex-1 flex items-center justify-center p-2">
+                        <FormLogin loginType="paciente" />
+                    </main>
+                </div>
             </div>
         </div>
     );
