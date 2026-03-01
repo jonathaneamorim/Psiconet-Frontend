572ed49968cc21fb81c98ebfebee67cebaa9b573
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-01 20:59:05 -0300

    feat: inserindo esquema de layout para login e cadastro de psicologo

---
+++ src/app/psicologo/(acess)/cadastro/page.tsx
@@ -0,0 +1,5 @@
+import { FormRegister } from "@/components/Organism/FormRegister";
+
+export default function PsicologoCadastro() {
+    return <FormRegister registerType="Psicologo" />;
+}
---
+++ src/app/psicologo/(acess)/login/page.tsx
@@ -0,0 +1,5 @@
+import { FormLogin } from "@/components/Organism/FormLogin";
+
+export default function PsicologoLogin() {
+    return <FormLogin loginType="Psicologo" />;
+}
